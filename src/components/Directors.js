c161dc5f7422ba1480bb8ccc79fea5713ee71445
winston kiprop <winstonekip50@gmail.com>
2022-07-23 14:30:06 +0300

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,22 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+ 
+  const directotsContainer = directors.map((director) => (
+    <div key={director.name}>
+      <h2>{director.name}</h2>
+      <ul>
+        {director.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return <div>
+    <h1>Directors Page</h1>
+    {directotsContainer}   
+  </div>
 }
 
 export default Directors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,28 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return <div className="navbar">
+  <NavLink exact to="/" activeStyle={{
+    background: "orange",
+  }}>
+    Home
+  </NavLink>
+  <NavLink exact to="/movies" activeStyle={{
+    background: "orange",
+  }}>
+    Movies
+  </NavLink>
+  <NavLink exact to="/directors" activeStyle={{
+    background: "orange",
+  }}>
+    Directors
+  </NavLink>
+  <NavLink exact to="/actors" activeStyle={{
+    background: "orange",
+  }}>
+    Actors
+  </NavLink>
+</div>;
 }
 
 export default NavBar;
